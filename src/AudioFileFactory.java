2329fdbe9b9a6244e1115f5c003ad9004476a924
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2024-05-11 19:02:41 +0200

    VA08

--- src/AudioFileFactory.java
+++ src/AudioFileFactory.java
@@ -1,9 +1,25 @@
+import java.util.Arrays;
 
 public class AudioFileFactory {
-
 	
-	static AudioFile createAudioFile(String path) {
-		return null;
+	public static AudioFile createAudioFile(String path) {
+		if(path == null) {
+			return null;
+		}
 		
+		String[] pathParts = path.split("\\."); // nur "." ist ein regulärer Ausdruck
+		String extension = pathParts[pathParts.length -1].toLowerCase();
+		
+		String[] wavExtensions = {"wav"};
+		if(Arrays.asList(wavExtensions).contains(extension)) {
+			return new WavFile(path);
+		}
+		
+		String[] taggedExtensions = {"ogg", "mp3"};
+		if(Arrays.asList(taggedExtensions).contains(extension)) {
+			return new TaggedFile(path);
+		}
+		
+		throw new RuntimeException("Unknown suffix for AudioFile \"" + path + "\"");
 	}
 }
--- src/PlayList.java
+++ src/PlayList.java
@@ -1,6 +1,10 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,88 +18,183 @@ import java.util.Scanner;
 
 
 public class PlayList {
+	
+	private LinkedList<AudioFile> audioFileList = new LinkedList<AudioFile>();
+	private int current; // Position in der Abspielliste
+	
 
 	public PlayList() {
-		
+		current = 0;
 	}
 	
 	public PlayList(String m3uPathname) {
 		this();
-		// ! was mit m3uPathname?
+		loadFromM3U(m3uPathname);
 	}
+	
 	
 	
 	public void add(AudioFile file) {
-		
+		audioFileList.add(file);
 	}
 	
 	public void remove(AudioFile file) {
-		
+		audioFileList.remove(file);
 	}
 	
 	public int size() {
-		return 0;
-		
+		return audioFileList.size();
 	}
 	
 	public AudioFile currentAudioFile() {
-		return null;
+		if(audioFileList.size() == 0) {
+			return null;
+		}
 		
+		return this.audioFileList.get(current);
 	}
 	
 	public void nextSong() {
-		
+		if(current > (audioFileList.size() -1)) {
+			// wenn Index auf einer ungültigen Position steht, dann 0
+			current = 0;
+		} else {
+			current = (current +1) % (audioFileList.size());	
+		}
 	}
 	
 	public void loadFromM3U(String pathname) {
-		File file = new File(pathname);
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(file);
-			while(scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				// ...
-				System.out.println(line);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally { // wird immer ausgeführt
-			scanner.close();
+		
+		// neu initialisieren
+		current = 0;
+		while(audioFileList.size() > 0) {
+			audioFileList.remove(0);
 		}
+		
+		
+		// code aus vorlesung
+//		File file = new File(pathname);
+//		Scanner scanner = null;
+//		try {
+//			scanner = new Scanner(file);
+//			while(scanner.hasNextLine()) {
+//				String line = scanner.nextLine();
+//				// ...
+//				System.out.println(line);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally { // wird immer ausgeführt
+//			scanner.close();
+//		}
+		
+		List<String> data = readFile(pathname);
+		
+		for(int i = 0; i < data.size(); i++) {
+			String line = data.get(i);
+			String path = line.substring(5);
+			if(!path.trim().equals("") && !path.substring(0, 1).equals("#")) {
+				audioFileList.add(AudioFileFactory.createAudioFile(path));
+			}
+		}
+		
+		System.out.println(audioFileList.size());
 	}
 	
 	public void saveAsM3U(String pathname) {
-		File file = new File(pathname);
+		// code aus vorlesung
+//		File file = new File(pathname);
+//		FileWriter writer = null;
+//		
+//		try {
+//			writer = new FileWriter(file);
+//			writer.write("Test \n");
+//			writer.write("Test \n");
+//			writer.write("Test \n");
+//			writer.close();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally { // wird immer ausgeführt
+//			try {
+//				writer.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		
+		String[] data = new String[audioFileList.size()];
+		for (int i = 0; i < audioFileList.size(); i++) {
+		    data[i] = audioFileList.get(i).getPathname();
+		}
+		
+		writeFile(pathname, data);
+	}
+	
+	public List<AudioFile> getList() {
+		return this.audioFileList;
+	}
+	
+	public int getCurrent() {
+		return this.current;
+	}
+	
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+	
+	
+	static private void writeFile(String path, String[] lines) {
+		// aus examples (Vorgabe)
 		FileWriter writer = null;
+		String sep = System.getProperty("line.separator");
 		
 		try {
-			writer = new FileWriter(file);
-			writer.write("Test \n");
-			writer.write("Test \n");
-			writer.write("Test \n");
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+			// create the file if it does not exist, otherwise reset the file and open it for writing
+			writer = new FileWriter(path);
+			for (String line : lines) {
+				// write the current line + newline char
+				writer.write(line + sep);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to write file " + path + "!");
 		} finally { // wird immer ausgeführt
 			try {
+				// close the file writing back all buffers
 				writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println("writeFile path: " + path + " error: " + e);
 			}
 		}
 	}
 	
-	public List<AudioFile> getList() {
-		return null;
-		
-	}
 	
-	public int getCurrent() {
-		return 0;
+	static private List<String> readFile(String path) {
+		List<String> lines = new ArrayList<>();
+		Scanner scanner = null;
 		
-	}
-	
-	public void setCurrent(int current) {
+		try {
+			// open the file for reading
+			scanner = new Scanner(new File(path));
+			int lineNo = 1;
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				// create line with line number prefix
+				String lineWithNumber = String.format("%03d: %s", lineNo++, line);
+				// add enhanced line to result list
+				lines.add(lineWithNumber);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				System.out.println("File " + path + " read!");
+				scanner.close();	
+			} catch (Exception e) {
+				System.out.println("readFile path: " + path + " error: " + e);
+			}
+		}
 		
+		return lines;
 	}
 }
